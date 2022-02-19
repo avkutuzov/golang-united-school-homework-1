325282fb45ce07739a59bced3a66d5f1ce6b9d75
artem.kutuzov <kutuzov_av@mail.ru>
2022-02-19 22:02:01 +0300

    add test solution

---
+++ solution/solution_test.go
@@ -0,0 +1,14 @@
+package solution_test
+
+import (
+	"quickstart/solution"
+	"testing"
+
+	"github.com/kyokomi/emoji"
+)
+
+func TestSolution(t *testing.T) {
+	if solution.GetMessage() != emoji.Sprint("Hello :world_map:!") {
+		t.Fatal("Wrong solution!")
+	}
+}
